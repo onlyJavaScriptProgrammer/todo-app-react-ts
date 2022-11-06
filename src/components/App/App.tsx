64dcee46a38e84c218cc7a63cd55bbe0b71dfad5
vladimir pasko <vp088897@gmail.com>
2022-11-06 14:09:09 +0300

    add conditional rendering

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import TodoPanel from '../TodoPanel/TodoPanel';
 import TodoList from '../TodoList/TodoList';
+import NotFound from '../NotFound/NotFound';
+import { useAppSelector } from '../../hooks';
+import type { RootState } from '../../store';
 import './App.scss';
 
 const App: React.FC = () => {
+  const isTodos = useAppSelector((state: RootState) => state.todos.list.length);
+
   return (
     <div className="container">
       <TodoPanel />
       <hr className="line" />
-      <TodoList />
+      {isTodos ? <TodoList /> : <NotFound />}
     </div>
   );
 };
---
+++ src/components/NotFound/NotFound.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './NotFound.scss';
+
+const NotFound = () => {
+  return <div className="notfound">Here is nothing Todo</div>;
+};
+
+export default NotFound;
